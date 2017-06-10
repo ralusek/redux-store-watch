08683a79487e59413e1069819d55fd5824e49cf2
ralusek <ralusek@gmail.com>
2017-06-10 10:35:19 -0700

    Added the ability to watch selectors. Updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### Watch paths on redux's store.
 
-The objective of this utility is to watch specific paths on redux's store. All
-paths's values are checked with strict equality. All data in a redux store
+The objective of this utility is to watch specific paths on redux's store. This
+is achieved by either providing a `selector` function whose results will be
+compared between current and previous state, or a string path whose value will
+be checked on the current and previous state.
+All differences are checked with strict equality. All data in a redux store
 should be edited immutably, which removes the need for anything but a strict
 equality check.
 
@@ -20,13 +23,15 @@ const watchStore = require('redux-store-watch');
 
 ##### Import your redux store. Assume imported as `store`.
 
-##### Using Redux component as example:
+### Using Redux component as example:
+
+##### Here is how we would watch some string path's on the store.
 ``` javascript
 class MyComponent extends Component {
   componentWillMount() {
     this.watcher = watchStore(store);
 
-    this.watcher('user.email', (current, previous, state) => {
+    this.watcher('user.email', (currentValue, previousValue, currentState, previousState) => {
       // store's `user.email` has changed
 
       // current is current value of user.email
@@ -34,7 +39,7 @@ class MyComponent extends Component {
       // state is the full current state.
     });
 
-    this.watcher('user.name', (current, previous, state) => {
+    this.watcher('user.name', (currentValue, previousValue, currentState, previousState) => {
       // store's `user.name` has changed
     });
 
@@ -42,7 +47,7 @@ class MyComponent extends Component {
     // on store's `user` path. Note that this watcher would also be called if either
     // of the previous watchers were called, as if `user.email` or `user.name` has
     // changed on an immutable store, `user` has necessarily changed.
-    this.watcher('user', (current, previous, state) => {
+    this.watcher('user', (currentValue, previousValue, currentState, previousState) => {
       // store's `user` has changed
     });
   }
@@ -51,6 +56,53 @@ class MyComponent extends Component {
     // We unsubscribe from the store. If you do not do this, in the same way as
     // if you had no unsubscribed from redux, the callbacks would continue to
     // be called even if the component were no longer mounted. 
+    this.watcher.remove();
+  }
+}
+```
+
+##### Here is how we would watch the results of a selector.
+``` javascript
+class MyComponent extends Component {
+  componentWillMount() {
+    this.watcher = watchStore(store);
+
+    // Let's say this is my selector, could be defined in another file. The
+    // selector will always be provided the current state as an argument.
+    const userEmailSelector = state => state.user.email;
+
+    this.watcher(userEmailSelector, (currentValue, previousValue, currentState, previousState) => {
+      // results of userEmailSelector has changed
+
+      // current is current value of the selector's result
+      // previous is previous value of selector's result
+      // state is the full current state.
+    });
+
+
+    // If you have a selector that requires specific arguments be passed in,
+    // you can simply wrap the selector in a closure.
+
+    // Say that this was my selector.
+    const userValueSelector = (prop, state) =>  state.user[prop];
+
+    // Because my selector is expecting something different than just the `state`
+    // argument passed in by the watcher, I can just wrap it in a function.
+    this.watcher(state => userValueSelector('email', state), (currentValue, previousValue, currentState, previousState) => {
+      // results of userValueSelector('email')
+    });
+
+
+    // As a last example, each selector's result is actually stored since the last
+    // change, so you could really put whatever you want as a function whose
+    // result you'd like to compare to last time a redux action was dispatched.
+    this.watcher(state => Math.random(), (currentValue, previousValue, currentState, previousState) => {
+      // A random number was different than the last time an action was dispatched...
+      // Absolutely fascinating.
+    });
+  }
+
+  componentWillUnmount() {
     this.watcher.remove();
   }
 }
--- index.js
+++ index.js
@@ -27,35 +27,120 @@ class StoreWatcher {
   constructor(store) {
     p(this).store = store;
 
-    p(this).watchPaths = {};
-    p(this).previous = store.getState();
+    p(this).watchedSelectors = new Map();
 
+    // Mapping paths to their selectors.
+    p(this).pathSelectorMapping = {};
+
+
+    // We store the previous state. Because the state should be immutable, this
+    // means we're only keeping at minimum a single object reference in memory,
+    // whereas most others remain unchanged references between states.
+    // This is used for diffing given paths.
+    p(this).previousState = store.getState();
+
+    // For selectors, we must store the previous results found for a given
+    // selector keyed by the selector itself.
+    p(this).previousSelectorValues = new WeakMap();
+
+
+    // Bind Private methods.
+    p(this).handleSelectorChanges = handleSelectorChanges.bind(this);
+
+
+    // Subscribe to store.
     p(this).unsubscribe = store.subscribe(() => {
-      const state = p(this).store.getState();
+      const currentState = p(this).store.getState();
+      const previousState = p(this).previousState;
 
-      const previous = p(this).previous;
-      p(this).previous = state;
+      // Redefine previous state as current state.
+      p(this).previousState = currentState;
 
-      for (let path in p(this).watchPaths) {
-        const changeListeners = p(this).watchPaths[path];
-
-        const previousValue = _get(previous, path);
-        const current = _get(state, path);
-
-        if (previousValue !== current) {
-          changeListeners.forEach(changeListener => changeListener(current, previousValue, state));
-        }
-      }
+      // On store action, handle changes to watched store selector.
+      p(this).handleSelectorChanges(currentState, previousState);
     });
   }
 
-  watch(path, changeListener) {
-    if (!path || (typeof path !== 'string')) throw new Error(`StoreWatcher must be given a valid path to watch. Got: ${path}.`);
-    p(this).watchPaths[path] = p(this).watchPaths[path] || [];
-    p(this).watchPaths[path].push(changeListener);
+
+  /**
+   * Watch a given string path or a selector's values on store action. Strict
+   * equality is used under the assumption that immutable store is employed.
+   */
+  watch(pathOrSelector, changeListener) {
+    if (!pathOrSelector) throw new Error('StoreWatcher watch must be provided pathOrSelector.');
+    if (!isFunction(changeListener)) throw new Error('StoreWatcher watch must be provided a function changeListener.');
+
+    // If we've been provided a string, we convert it to a selector.
+    if (isString(pathOrSelector)) {
+      const path = pathOrSelector;
+      const mapping = p(this).pathSelectorMapping;
+      // Create or find existing selector for the given path.
+      if (!mapping[path]) {
+        // Create selector and add mapping of path to the selector to avoid
+        // having to create multiple selectors if multiple parties interested in same path.
+        mapping[path] = (state) => _get(state, path);
+      }
+      pathOrSelector = mapping[path];
+    }
+
+    if (isFunction(pathOrSelector)) {
+      const selector = pathOrSelector;
+      if (!p(this).watchedSelectors.get(selector)) {
+        p(this).watchedSelectors.set(selector, []);
+      }
+      p(this).watchedSelectors.get(selector).push(changeListener);
+      // Set selector result as previous selector value.
+      p(this).previousSelectorValues.set(selector, selector());
+    }
   }
 
+
+  /**
+   * Unsubscribe from store.
+   */
   remove() {
     p(this).unsubscribe();
   }
+}
+
+
+// Private methods.
+
+/**
+ * Check for change between previous selector result and current selector result.
+ * Something of note is that the previous state value is explicitly saved, even
+ * though it could just be checked again by selecting from the previousState.
+ * The reason this is is because it is a very small memory footprint, likely
+ * just reference to a part of the state already in memory, which provides the
+ * additional benefit of letting your selector reference some other arbitrary
+ * state. If all you care about is checking if something has changed since then,
+ * you may do so.
+ */
+function handleSelectorChanges(currentState, previousState) {
+  p(this).watchedSelectors.forEach((changeListeners, selector) => {
+    const currentValue = selector(currentState);
+    const previousValue = p(this).previousSelectorValues.get(selector);
+
+    // Redefine previous selector's result to reflect current selector.
+    p(this).previousSelectorValues.set(selector, currentValue);
+
+    if (previousValue !== currentValue) {
+      changeListeners.forEach(changeListener => {
+        changeListener(currentValue, previousValue, currentState, previousState);
+      });
+    }
+  });
+}
+
+
+
+// Utilities.
+const asString = Object.prototype.toString;
+
+function isString(subject) {
+  return asString.call(subject) == '[object String]';
+}
+
+function isFunction(subject) {
+  return asString.call(subject) == '[object Function]';
 }
